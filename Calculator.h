e47886440963fd9b5862bb8ce33347de269b34b6
Sandu Kiritsa <kiritasandu@gmail.com>
2021-06-10 17:15:09 +0300

    cleanup

--- Calculator.h
+++ Calculator.h
@@ -19,28 +19,6 @@ struct Token {
 };
 
 class Calculator {
-  static bool AddCatchOverflow(int64_t *result, int64_t a, int64_t b) {
-    *result = a + b;
-    if (a > 0 && b > 0 && *result < 0)
-      return true;
-    if (a < 0 && b < 0 && *result > 0)
-      return true;
-    return false;
-  }
-  static bool MultiplyCatchOverflow(int64_t *result, int64_t a, int64_t b) {
-    *result = a * b;
-    if (a > 0 && b > 0 && *result < 0)
-      return true;
-    if (a < 0 && b < 0 && *result < 0)
-      return true;
-    if (a > 0 && b < 0 && *result > 0)
-      return true;
-    if (a < 0 && b > 0 && *result < 0)
-      return true;
-
-    return false;
-  }
-
   static int OperatorPriority(const std::string &ch);
   static bool IsNumber(const std::string &s);
   static Result<std::vector<Token>> InfixToPostfix(const std::vector<Token> &infix);
